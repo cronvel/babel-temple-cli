674369fc48e7bca1e0e0939d81dd8352deba8f0a
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2019-09-21 12:13:07 +0200

    Dependencies upgraded

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,9 +26,10 @@ module.exports = {
 		'no-unneeded-ternary': 'error' ,
 		'no-unused-vars': 'warn' ,	// During development phase, it's boring to clean unused var since they can be used later
 		'no-lonely-if': 'error' ,
-		'no-nested-ternary': 'error' ,
+		'no-nested-ternary': 'off' ,	// Now I use the streamlined ternary operator a lot
 		'no-shadow': 'warn' ,
 		'no-shadow-restricted-names': 'error' ,
+		'require-atomic-updates': 'off' ,	// check for possible race condition on assignment, interesting but too nitpicky
 		
 		
 		
@@ -128,8 +129,9 @@ module.exports = {
 				'minProperties': 4
 			} ,
 			'ObjectPattern' : {
+				// object destructuring assigment
 				'consistent': true ,
-				'minProperties': 6
+				'minProperties': 8
 			}
 		} ] ,
 		'object-curly-spacing': [ 'error' , 'always' ] ,
